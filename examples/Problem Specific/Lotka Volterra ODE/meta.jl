5ea4a73d9dc5949834d5d639fbf47194259060fe
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2025-01-29 11:28:22 +0300

    wip

--- examples/Problem Specific/Lotka Volterra ODE/meta.jl
+++
@@ -1,7 +0,0 @@
-return (
-    title = "Lotka Volterra ODE",
-    description = """
-    An example of solving Lotka Volterra ODE using DifferentialEquations.jl. Reference: [Lotka Volterra ODE](https://en.wikipedia.org/wiki/Lotka%E2%80%93Volterra_equations).
-    """,
-    tags = ["problem specific", "ode", "differential equations"]
-)
