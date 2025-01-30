f77e65aa35729e87c33266dd430072c59e9d032a
Bagaev Dmitry <bvdmitri@gmail.com>
2025-01-30 08:18:07 +0100

    Update meta.jl

--- examples/Problem Specific/ODE Parameter Estimation/meta.jl
+++ examples/Problem Specific/ODE Parameter Estimation/meta.jl
@@ -1,7 +1,7 @@
 return (
-    title = "Lotka Volterra ODE",
+    title = "ODE Parameter Estimation",
     description = """
-    An example of solving Lotka Volterra ODE using DifferentialEquations.jl. Reference: [Lotka Volterra ODE](https://en.wikipedia.org/wiki/Lotka%E2%80%93Volterra_equations).
+    An example of solving Lotka Volterra ODE with RxInfer.jl. Reference: [Lotka Volterra ODE](https://en.wikipedia.org/wiki/Lotka%E2%80%93Volterra_equations).
     """,
     tags = ["problem specific", "ode", "differential equations"]
 )
