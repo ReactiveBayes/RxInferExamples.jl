23da4e2739ecaa2489c5b1af1cb5f6da66f9e3c3
Bagaev Dmitry <bvdmitri@gmail.com>
2025-02-06 23:00:09 +0100

    oopss..

--- examples/Basic Examples/Feature Functions in Bayesian Regression/meta.jl
+++ examples/Basic Examples/Feature Functions in Bayesian Regression/meta.jl
@@ -1,5 +1,5 @@
 return (
-    title = "Parametric Gaussian Regression",
+    title = "Feature Functions in Bayesian Regression",
     description = """
     An example of Bayesian inference in a parametric Gaussian regression model.
     Based on "Probabilistic Numerics: Computation as Machine Learning" by Hennig, Osborne and Kersting.
