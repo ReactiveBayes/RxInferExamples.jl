9bf0338efae951fe594e7962d96382bc51f2a588
Albert <albert.podusenko@gmail.com>
2025-03-03 14:44:13 +0100

    Add robotic arm example

---
+++ examples/Advanced Examples/Robotic Arm/meta.jl
@@ -0,0 +1,7 @@
+return (
+    title = "Robotic Arm",
+    description = """
+    This example shows how to use RxInfer.jl automated inference to do path planning for a robotic arm.
+    """,
+    tags = ["advanced examples"]
+)
