bc3aacbfaa1939cbaf10f4488be552aa7bce3517
Documenter.jl <documenter@juliadocs.github.io>
2025-03-07 13:50:22 +0000

    build based on 9bf5ceb

---
+++ categories/advanced_examples/drone_dynamics/meta.jl
@@ -0,0 +1,7 @@
+return (
+    title = "Drone Dynamics",
+    description = """
+    This example shows how to use RxInfer.jl automated inference to simulate drone dynamics.
+    """,
+    tags = ["advanced examples"]
+)
---
+++ categories/advanced_examples/multi-agent_trajectory_planning/meta.jl
@@ -0,0 +1,7 @@
+return (
+    title = "Multi-agent Trajectory Planning",
+    description = """
+    This example shows how to plan multi-agents' trajectories while avoiding obstacles and collisions between agents.
+    """,
+    tags = ["advanced examples"]
+)
