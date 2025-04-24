f8ed683cf1aca789250dfe1e746d6ae1926e8207
Albert <albert.podusenko@gmail.com>
2025-04-24 12:44:08 +0200

    Add collab

---
+++ examples/Basic Examples/Collaboration and Matching/meta.jl
@@ -0,0 +1,7 @@
+return (
+    title = "Collaboration and Matching",
+    description = """
+    An example of matchmaking for collaborations.
+    """,
+    tags = ["collaboration", "matchmaking"]
+)
