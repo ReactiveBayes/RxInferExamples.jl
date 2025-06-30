466c925c53ff70b9e3f3d1e7e700121ff9e9fd21
Bagaev Dmitry <bvdmitri@gmail.com>
2025-06-30 09:42:15 +0000

    build based on b00f314

---
+++ categories/advanced_examples/integrating_neural_networks_with_flux.jl/meta.jl
@@ -0,0 +1,7 @@
+return (
+    title = "Integrating Neural Networks with Flux.jl",
+    description = """
+    This example shows how to use RxInfer.jl together with Flux.jl to incorporate neural networks into probabilistic models.
+    """,
+    tags = ["advanced examples", "neural networks", "deep learning", "integration", "Flux.jl"]
+)
