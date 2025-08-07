d54cc1fe7ed56220d1d6352521e1c6f29dc9c3fb
Albert <albert.podusenko@gmail.com>
2025-08-07 21:13:47 +0200

    Add example on integrating LLMs with RxInfer

---
+++ examples/Experimental Examples/Large Language Models/meta.jl
@@ -0,0 +1,11 @@
+meta = (
+    title = "Large Language Models",
+    description = """
+        This is an experimental example of a Large Language Model (LLM) integration with RxInfer.
+    """,
+    tags = [
+        "experimental examples",
+        "large language models",
+        "llm",
+    ]
+)
