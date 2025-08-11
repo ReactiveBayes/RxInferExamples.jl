b8b56a15f5d43af3bcd8e6612eec18f964648b5f
Bagaev Dmitry <bvdmitri@gmail.com>
2025-08-11 14:18:17 +0000

    build based on 23c3c61

---
+++ categories/advanced_examples/learning_dynamics_with_vaes/meta.jl
@@ -0,0 +1,7 @@
+return (
+    title = "Learning Dynamics with VAEs",
+    description = """
+    This example shows how to use RxInfer.jl automated inference to learn dynamics in latent space of a VAE.
+    """,
+    tags = ["advanced examples", "vae", "dynamics"]
+)
