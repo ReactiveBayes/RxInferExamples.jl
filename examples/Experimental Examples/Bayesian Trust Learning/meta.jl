5bdc398ffe37135e41f5758aa6eea3c6eec29ac8
Albert <albert.podusenko@gmail.com>
2025-09-30 23:32:29 +0200

    Add Trust example

---
+++ examples/Experimental Examples/Bayesian Trust Learning/meta.jl
@@ -0,0 +1,15 @@
+meta = (
+    title = "Bayesian Trust Learning",
+    description = """
+        This is an experimental example of a Bayesian Trust Learning for LLM Routing.
+    """,
+    tags = [
+        "experimental examples",
+        "llm routing",
+        "bayesian trust learning",
+        "hierarchical bayesian models",
+        "custom nodes",
+        "custom rules",
+        "online learning cycles",
+    ]
+)
