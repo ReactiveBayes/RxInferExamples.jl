5915a3ccb6ca985dfbb2b57d5bf7bc5fdb15aeab
Carlo Nucera <meditans@gmail.com>
2026-02-12 14:14:58 +0100

    Add EFE Minimization via Message Passing notebook
    Add a tutorial recreation of the paper
    A Message Passing Realization of Expected Free Energy Minimization
    by Wouter Nuijten, Mykola Lukashchuk, Thijs van de Laar and Bert de Vries

---
+++ examples/Advanced Examples/EFE Minimization via Message Passing/meta.jl
@@ -0,0 +1,7 @@
+return (
+    title = "EFE Minimization via Message Passing",
+    description = """
+    This example shows how to minimize Expected Free Energy inside the RxInfer framework.
+    """,
+    tags = ["advanced examples"]
+)
